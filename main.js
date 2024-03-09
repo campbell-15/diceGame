b782e36fcd43ed96d6539fb67a6b4149455d10cb
Campbell Davies-Webb <campbelldavieswebb@gmail.com>
2024-03-09 15:23:33 +0200

    Roll dice function

--- main.js
+++ main.js
@@ -19,5 +19,30 @@ function editNames() {
 }
 
 function rollDice() {
-    
+    let diceNum1 = document.querySelector(".img1")
+    let diceNum2 = document.querySelector(".img2")
+
+    diceNum1.setAttribute("src", "dice-roll.gif")
+    diceNum2.setAttribute("src", "dice-roll.gif")
+
+    let result = document.querySelector('h1')
+    setTimeout(() => {
+        let randomNum1 = math.floor(math.random() *6)+1;
+        let randomNum2 = math.floor(math.random() *6)+1;
+
+        diceNum1.setAttribute('src', 'dice'+randomNum1+'.png');
+        diceNum2.setAttribute('src', 'dice'+randomNum2+'.png');
+
+        // winner method
+        if (randomNum1 === randomNum2) {
+            result.innerHTML = "Draw!"
+        }
+        else if (randomNum1 < randomNum2) {
+            result.innerHTML = (player2 + " WINS!");
+        }
+        else {
+            result.innerHTML = (player1 + " WINS!");
+        }
+
+    }, 2500);
 }
