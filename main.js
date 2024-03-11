96046c876e76918039ba272331304624c68ce89c
Campbell Davies-Webb <campbelldavieswebb@gmail.com>
2024-03-11 21:03:35 +0200

    edit names comments

--- main.js
+++ main.js
@@ -1,17 +1,21 @@
+// initial player names and leaderboard object
 let player1 = "Player1";
 let player2 = "Player2";
 let leaderboard = {};
 
 // Edit names function
 function editNames() {
+    // Prompt users to enter player names
     player1 = prompt("Please enter player 1's name");
     player2 = prompt("Please enter player 2's name");
 
+    // Check if both names are valid
     if (!player1 || !player2) {
         alert("Please enter valid names for both players.");
         return;
     }
 
+    // Update player names displayed on the webpage
     document.querySelector("p.Player1").innerHTML = player1;
     document.querySelector("p.Player2").innerHTML = player2;
 }
